8f99b2e7bd8e433563892386fd9b6bec1f8d82a3
Alexander Roosma <alex.roosma@outlook.com>
2022-12-04 10:38:51 -0600

    day 3 - more attempts and validation

--- day_03/day03-1.py
+++ day_03/day03-1.py
@@ -1,4 +1,4 @@
-file = open("day_03_input_test.txt", "r")
+file = open("day_03_input.txt", "r")
 data = file.read().strip().split("\n")
 
 rucksacks = []
@@ -14,26 +14,60 @@ for _l in letters:
 print(priority_values)
 
 for line in data:
-    rucksacks.append([line[:int(len(line)/2 - 1)], line[int(len(line)/2):]])
-
-_c = '1'
+    rucksacks.append([line[:int(len(line)/2)], line[int(len(line)/2):]])
+_c = '??'
 sum = 0
 type_set = set()
 
+line_no = 1
+
+same_count_list = []
+
+match_dict = dict()
+
 for _sack in rucksacks:
+    
+    _same_count_set = set()
     for _i in _sack[0]:
         if _i in _sack[1]:
             _c = _i
-            break
+            #break
+            _same_count_set.add(_i)
+            
+    for _x in _sack[0]:
+        for _y in _sack[1]:
+            if _x == _y:
+                match_dict[_x] = 1
+
+    same_count_list.append(len(_same_count_set))
+
     prev_len = len(type_set)
     type_set.add(_c)
+    print(f"Line #{line_no}\t", end="")
+    line_no+=1
+
+    print(f"1st Half: {_sack[0]} \t2nd Half: {_sack[1]}")
+
     if len(type_set) == prev_len:
         print(f"{_c} already in set.")
     else:
         print(f"{_c} added to set")
     #print(f"Shared char: {_c}")
 
+
+
 print(type_set)
+print(match_dict.keys())
+
+try1 = list(type_set)
+try2 = list(match_dict.keys())
+
+try1.sort()
+try2.sort()
+
+print(try1)
+print(try2)
+
 
 for _c in type_set:   
     char_priority = 0
@@ -53,5 +87,5 @@ for _t in type_set:
     sum2 += priority_values[_t]
 
 print(f"Custom count: {sum2}")
-
+print(same_count_list)
 file.close()
