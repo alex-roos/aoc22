332ef003afd7a603c0499e620562af5aa9608d74
Alexander Roosma <alex.roosma@outlook.com>
2022-12-04 11:17:11 -0600

    day 04 - broken, first attempt

---
+++ day_04/day04-1.py
@@ -0,0 +1,62 @@
+file = open("day_04_input.txt", "r")
+data = file.read().strip().split("\n")
+file.close()
+
+write_file = open("day04_output.txt", "a")
+
+elf_pairs = []
+
+for line in data:
+    unparsed_elf_1, unparsed_elf_2 = line.split(',')
+
+    elf_pairs.append([([int(_x) for _x in unparsed_elf_1.split('-')]), ([int(_x) for _x in unparsed_elf_2.split('-')])])
+
+count_overlaps = 0
+
+line_no = 1
+
+for pair in elf_pairs:
+    prev_count_overlaps = count_overlaps
+
+    elf1_sect_start, elf1_sect_end = pair[0]
+    elf2_sect_start, elf2_sect_end = pair[1]
+
+    output_array_1 = 100 * ['--']
+    output_array_2 = 100 * ['--']
+
+    for _i in range(elf1_sect_start, elf1_sect_end):
+        if _i > 9:
+            output_array_1[_i] = str(_i)
+        else:
+            output_array_1[_i] = "0" + str(_i)
+
+    for _i in range(elf2_sect_start, elf2_sect_end):
+        if _i > 9:
+            output_array_2[_i] = str(_i)
+        else:
+            output_array_2[_i] = "0" + str(_i)
+
+    if elf1_sect_start <= elf2_sect_start:
+        if elf1_sect_end >= elf2_sect_end:
+            count_overlaps += 1
+            print(pair)
+    
+    if elf2_sect_start <= elf1_sect_start:
+        if elf2_sect_end >= elf1_sect_end:
+            count_overlaps += 1
+            print(pair)
+
+    if prev_count_overlaps ==count_overlaps:
+        write_file.write(f"Line {line_no} for {pair}\n")
+        write_file.write(' '.join(output_array_1))
+        write_file.write("\n")  
+        write_file.write(' '.join(output_array_2))
+        write_file.write("\n")
+
+    line_no += 1
+
+
+# 416 too low, 511 too high
+print(f"Total full contains: {count_overlaps}")
+
+write_file.close()
