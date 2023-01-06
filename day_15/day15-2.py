bfbfb5a268fca35bc022aa4f0089bd417ac58c67
Alexander Roosma <alex.roosma@outlook.com>
2023-01-06 09:31:03 -0600

    day 15 - pt 2 row by row solution

--- day_15/day15-2.py
+++ day_15/day15-2.py
@@ -1,15 +1,21 @@
-global GLOBAL_ROWS, MAX_VALUE
-MAX_VALUE = 4000000
-GLOBAL_ROWS = [[-1] for _ in range(MAX_VALUE+1)]
+global GLOBAL_ROWS, MAX_VALUE, SENSOR_ARRAY
+MAX_VALUE = 20
+GLOBAL_ROW = [0] * (MAX_VALUE + 1)
+
+# 0: x, 1: y, 2: range
+SENSOR_ARRAY = []
 
 class Sensor:
-    x = None
+    x = int()
     y = None
 
-    beacon_x = None
-    beacon_y = None
+    beacon_x = int()
+    beacon_y = int()
 
-    dist_covered = None
+    min_row_reached = int()
+    max_row_reached = int()
+
+    dist_covered = int()
 
     def __init__(self, _s_x, _s_y, _b_x, _b_y) -> None:
         self.x = _s_x
@@ -18,68 +24,29 @@ class Sensor:
         self.beacon_y = _b_y
 
         self.dist_covered = abs(_s_x-_b_x) + abs(_s_y - _b_y)
-        self.determineCoverage()
 
-    def determineCoverage(self) -> None:
-        global GLOBAL_ROWS
+        self.min_row_reached = self.y - self.dist_covered
+        self.max_row_reached = self.y + self.dist_covered
 
-        self.locs_covered = set()
+    def determineCoverage(self, concerned_row) -> None:
+        global GLOBAL_ROW
 
-        print(f"----- Sensor: [{self.x},{self.y}] ------")
+        #print(f"----- Sensor: [{self.x},{self.y}] ------")
 
-        for _y_row in range(self.y - self.dist_covered, self.y + self.dist_covered + 1):
-            if self.x == 1557973 and _y_row > 2708300 and _y_row % 10 == 0:
-                print(_y_row)
-                print(GLOBAL_ROWS[_y_row-1])
-            if _y_row >= 0 and _y_row <= MAX_VALUE:
-                _remaining_dist = self.dist_covered - abs(self.y - _y_row)
-                row_x_start = self.x - _remaining_dist
-                if row_x_start < 0:
-                    row_x_start = 0
-                row_x_end = self.x + _remaining_dist
-                if row_x_end > MAX_VALUE:
-                    row_x_end = MAX_VALUE
+        # look ONLY at ONE ROW
+        _remaining_dist = self.dist_covered - abs(self.y - concerned_row)
+        if _remaining_dist > 0:
+            row_x_start = self.x - _remaining_dist
+            if row_x_start < 0:
+                row_x_start = 0
+            row_x_end = self.x + _remaining_dist
+            if row_x_end > MAX_VALUE:
+                row_x_end = MAX_VALUE
 
-                if type(GLOBAL_ROWS[_y_row][0]) == int:
-                    # no info yet for this row
-                    GLOBAL_ROWS[_y_row] = [[row_x_start, row_x_end]]
-                else:
+            for _x in range(row_x_start, row_x_end+1):
+                GLOBAL_ROW[_x] = 1
 
-                    GLOBAL_ROWS[_y_row].sort()
-
-                    _temp_line_seg = [0] * (MAX_VALUE + 1)
-                    for _i in range(row_x_start, row_x_end+1):
-                        _temp_line_seg[_i] = 1
-
-                    for _segment in GLOBAL_ROWS[_y_row]:
-                        for _i in range(_segment[0], _segment[1]+1):
-                            _temp_line_seg[_i] = 1
-
-                    _segment_list = []
-
-                    parse_state = "looking"
-
-                    curr_x_start = -1
-
-                    for _i in range(len(_temp_line_seg)):
-                        if parse_state == "looking":
-                            if _temp_line_seg[_i] == 1:
-                                curr_x_start = _i
-                                parse_state = "online"
-                        elif parse_state == "online":
-                            if _temp_line_seg[_i] == 0:
-                                _segment_list.append([curr_x_start, _i-1])
-                                parse_state = "looking"
-                                curr_x_start = -1
-
-                    if parse_state == "online":
-                        _segment_list.append([curr_x_start,MAX_VALUE])
-
-                    GLOBAL_ROWS[_y_row] = _segment_list
-
-                #print(f"\tRow {_y_row}: from {row_x_start} to {row_x_end}")
-
-file = open(r"C:\Users\ARoos\dev\git\aoc22\day_15\day_15_input.txt", "r")
+file = open(r"C:\Users\ARoos\dev\git\aoc22\day_15\day_15_input_test.txt", "r")
 data = file.read().strip().split("\n")
 file.close()
 
@@ -94,9 +61,34 @@ for line in data:
     _beacon_x = int(tokens[3].split(',')[0])
     _beacon_y = int(tokens[4])
 
-    _next_sensor = Sensor(_sensor_x, _sensor_y, _beacon_x, _beacon_y)
+    sensor_list.append(Sensor(_sensor_x, _sensor_y, _beacon_x, _beacon_y))
 
+row_id = 0
+found_incomplete_row = False
 
-for _row in GLOBAL_ROWS:
-    if len(_row) > 1:
-        print(_row)
+empty_x = -1
+empty_y = -1
+
+while not found_incomplete_row and row_id <= MAX_VALUE:
+    GLOBAL_ROW = [0] * (MAX_VALUE + 1)
+
+    for _sensor in sensor_list:
+        _sensor.determineCoverage(row_id)
+        # if _sensor.y == 11:
+        #     print(GLOBAL_ROW)
+
+    # if row_id % 100000 == 0:
+    #     print(f"Row {row_id}: sum = {sum(GLOBAL_ROW)}")
+
+    if sum(GLOBAL_ROW) != MAX_VALUE + 1:
+        found_incomplete_row = True
+        print(f"Found incomplete row # {row_id}.")
+        empty_y = row_id
+
+    row_id += 1
+
+for _i in range(len(GLOBAL_ROW)):
+    if GLOBAL_ROW[_i] == 0:
+        empty_x = _i
+
+print(f"Opening at x: {empty_x}, y: {empty_y}")
